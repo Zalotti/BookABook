a213104cb9d798bf201639c0a45a701156440894
Zalotti <38702089+Zalotti@users.noreply.github.com>
2023-09-22 08:22:51 -0300

    Delete BookABook-main/BookABookProject directory

--- BookABook-main/BookABookProject/BookABook/src/main/java/br/edu/ifsp/arq/dw2s6/bookabook/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifsp.arq.dw2s6.bookabook.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifsp.arq.dw2s6.bookabook.domain.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review,Long>{
-
-}
