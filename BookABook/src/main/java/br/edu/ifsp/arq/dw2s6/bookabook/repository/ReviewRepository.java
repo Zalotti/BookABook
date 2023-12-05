64bc8552ebc1c6a6be996f221cb565ef71118daa
Zalotti <38702089+Zalotti@users.noreply.github.com>
2023-12-05 11:14:52 -0300

    Adicionado FindByUser

--- BookABook/src/main/java/br/edu/ifsp/arq/dw2s6/bookabook/repository/ReviewRepository.java
+++ BookABook/src/main/java/br/edu/ifsp/arq/dw2s6/bookabook/repository/ReviewRepository.java
@@ -3,7 +3,10 @@ package br.edu.ifsp.arq.dw2s6.bookabook.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifsp.arq.dw2s6.bookabook.domain.model.Review;
+import br.edu.ifsp.arq.dw2s6.bookabook.domain.model.User;
 
 public interface ReviewRepository extends JpaRepository<Review,Long>{
+
+  public List<Review> findByUser(User user);
 
 }
