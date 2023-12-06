3003e8de7afb66fc61c36e80c8698b38f03d0940
Zalotti <38702089+Zalotti@users.noreply.github.com>
2023-12-05 22:16:03 -0300

    Correção dos Imports

--- BookABook/src/main/java/br/edu/ifsp/arq/dw2s6/bookabook/repository/ReviewRepository.java
+++ BookABook/src/main/java/br/edu/ifsp/arq/dw2s6/bookabook/repository/ReviewRepository.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.arq.dw2s6.bookabook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifsp.arq.dw2s6.bookabook.domain.model.Review;
